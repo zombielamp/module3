6ff71865cec5e1bae2c3ec1f000d43db5e72bd7a
zombielamp <109299081+zombielamp@users.noreply.github.com>
2022-10-22 15:43:54 +0500

    Update 4,1.py

--- 4,1.py
+++ 4,1.py
@@ -1,13 +1,15 @@
 import json
 
-login = input('Введите логин')
-passwd = input('Введите пароль')
+login = {'Login' : input('Введите логин ')}
+passwd = {'Password' : input('Введите пароль ')}
 
 with open ('register.json', 'w') as f:
-		json.dump(login, f)
-		json.dump(passwd, f)
+	json.dump(login, f)
+	json.dump(passwd, f)
 
 def register(login, passwd):
-	print (login, passwd)
+	with open ('register.json', 'r') as f:
+		login = json.load(f)
+		passwd = json.load(f)
 
-register(login,passwd)
+	print (login, passwd)
