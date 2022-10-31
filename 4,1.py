819ee07cf5047051262e134d9ac9619610e7c3ca
zombielamp <109299081+zombielamp@users.noreply.github.com>
2022-10-31 16:17:13 +0500

    Update 4,1.py

--- 4,1.py
+++ 4,1.py
@@ -1,15 +1,31 @@
 import json
 
-name = [input('Введите логин ')]
-password = [input('Введите пароль ')]
-with open('reg.json', 'w') as f:
-	json.dump(name, f)
-	json.dump(password, f)
+data = {'login': 'paswd'}
+with open('reg.json', 'a') as f:
+	json.dump(data, f)
 
+def register(login, psswd):
+	with open('reg.json', 'r') as f:
+		data = json.load(f)
+	if login in data.keys():
+		print('Данный логин уже существует. Придумайте новый логин.')
+	else:
+		data[login] = psswd
+		with open('reg.json', 'w') as f:
+			json.dump(data,f)
+		print(' Регистрация прошла успешно!')
+		
 
-def register(login, passwd):
-	if login in name:
-		if passwd in password:
-			print(name, password)
-
-print(name, password)
+def login_function(login, psswd):
+	with open('reg.json', 'r') as f:
+		data = json.load(f)
+	while true:
+		login = input('Введите логин ')
+		passwd = input('Введите пароль ')
+		if login and passwd in data.keys():
+			data[login] == data[passwd]
+			with open('reg.json', 'w') as f:
+				json.dump(data,f)
+			print('Добро пожаловать!')
+		else:
+			print('Неверный логин или пароль')
